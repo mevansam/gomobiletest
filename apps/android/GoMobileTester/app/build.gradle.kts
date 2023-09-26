1a43b8c20e10cb0b064a6d8bf77e9fe6f30b40bb
Mevan Samaratunga <mevansam@gmail.com>
2023-09-26 13:23:20 -0400

    formatting for clarity

--- apps/android/GoMobileTester/app/build.gradle.kts
+++ apps/android/GoMobileTester/app/build.gradle.kts
@@ -48,6 +48,8 @@ android {
 
 dependencies {
 
+    implementation(files("../../../../shared/android/User.aar"))
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
@@ -56,7 +58,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation(files("../../../../shared/android/User.aar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
