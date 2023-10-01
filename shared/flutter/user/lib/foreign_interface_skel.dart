ba5aefac7c3dfb4c4de638151f978f14a34fd4f2
Mevan Samaratunga <mevansam@gmail.com>
2023-10-01 14:30:47 -0400

    fixed template type name caps

--- shared/flutter/user/lib/foreign_interface_skel.dart
+++ shared/flutter/user/lib/foreign_interface_skel.dart
@@ -8,25 +8,25 @@ import 'package:ffi/ffi.dart';
 // managing cross-references with the foreign
 // interface stups as well as boiler plate code
 // for dart ffi bookeeping.
-abstract class ForeignInterfaceSkel<FOREIGN_T extends ffi.Struct> {
+abstract class ForeignInterfaceSkel<Foreign_T extends ffi.Struct> {
   // The handle to this skeleton passed to the
   // foreign code.
-  ffi.Pointer<FOREIGN_T> get handle => _handle;
+  ffi.Pointer<Foreign_T> get handle => _handle;
 
   // Looks up the skeleton instance when foreign code
   // calls dart code to invoke a function provide by
   // the skeleton implementation.
-  static DART_T lookupInstance<DART_T>(int context) {
-    return ForeignInterfaceSkel._handlers[context]! as DART_T;
+  static Dart_T lookupInstance<Dart_T>(int context) {
+    return ForeignInterfaceSkel._handlers[context]! as Dart_T;
   }
 
   // The implementing stub should implement this function.
   // It should create and initialize the ffi struct that
   // will be used as the handle for the foreign code to
   // invoke the dart code.
-  ffi.Pointer<FOREIGN_T> create();
+  ffi.Pointer<Foreign_T> create();
 
-  late final ffi.Pointer<FOREIGN_T> _handle;
+  late final ffi.Pointer<Foreign_T> _handle;
   static final Map<int, ForeignInterfaceSkel> _handlers =
       <int, ForeignInterfaceSkel>{};
 
