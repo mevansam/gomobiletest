37e52fe36d8d0d2293c49d0c9e63a7cc17a156b2
Mevan Samaratunga <mevansam@gmail.com>
2023-10-02 00:07:28 -0400

    optimized with async runner

--- shared/flutter/user/lib/sum_example.dart
+++ shared/flutter/user/lib/sum_example.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:isolate';
 
+import 'async_runner.dart';
 import 'user_bindings.dart' as bindings;
 
 /// A very short-lived native function.
@@ -21,88 +22,34 @@ int sum(int a, int b) => bindings.user.sum(a, b);
 /// 1. Reuse a single isolate for various different kinds of requests.
 /// 2. Use multiple helper isolates for parallel execution.
 Future<int> sumAsync(int a, int b) async {
-  final SendPort helperIsolateSendPort = await _helperIsolateSendPort;
-  final int requestId = _nextSumRequestId++;
-  final _SumRequest request = _SumRequest(requestId, a, b);
+  AsyncRunner asyncRunner = AsyncRunner();
   final Completer<int> completer = Completer<int>();
-  _sumRequests[requestId] = completer;
-  helperIsolateSendPort.send(request);
+
+  asyncRunner
+      .run<_SumAsyncRequest, _SumAsyncResponse>(_SumAsyncRequest(a, b))
+      .then((response) {
+    completer.complete(response.result);
+    asyncRunner.dispose();
+  });
+
   return completer.future;
 }
 
-/// A request to compute `sum`.
-///
-/// Typically sent from one isolate to another.
-class _SumRequest {
-  final int id;
+class _SumAsyncRequest extends AsyncRequest {
+  _SumAsyncRequest(this.a, this.b);
+
   final int a;
   final int b;
 
-  const _SumRequest(this.id, this.a, this.b);
+  @override
+  AsyncResponse execute() {
+    final int result = bindings.user.sum_long_running(a, b);
+    return _SumAsyncResponse(this, result);
+  }
 }
 
-/// A response with the result of `sum`.
-///
-/// Typically sent from one isolate to another.
-class _SumResponse {
-  final int id;
+class _SumAsyncResponse extends AsyncResponse {
   final int result;
 
-  const _SumResponse(this.id, this.result);
+  _SumAsyncResponse(AsyncRequest request, this.result) : super(request);
 }
-
-/// Counter to identify [_SumRequest]s and [_SumResponse]s.
-int _nextSumRequestId = 0;
-
-/// Mapping from [_SumRequest] `id`s to the completers corresponding to the correct future of the pending request.
-final Map<int, Completer<int>> _sumRequests = <int, Completer<int>>{};
-
-/// The SendPort belonging to the helper isolate.
-Future<SendPort> _helperIsolateSendPort = () async {
-  // The helper isolate is going to send us back a SendPort, which we want to
-  // wait for.
-  final Completer<SendPort> completer = Completer<SendPort>();
-
-  // Receive port on the main isolate to receive messages from the helper.
-  // We receive two types of messages:
-  // 1. A port to send messages on.
-  // 2. Responses to requests we sent.
-  final ReceivePort receivePort = ReceivePort()
-    ..listen((dynamic data) {
-      if (data is SendPort) {
-        // The helper isolate sent us the port on which we can sent it requests.
-        completer.complete(data);
-        return;
-      }
-      if (data is _SumResponse) {
-        // The helper isolate sent us a response to a request we sent.
-        final Completer<int> completer = _sumRequests[data.id]!;
-        _sumRequests.remove(data.id);
-        completer.complete(data.result);
-        return;
-      }
-      throw UnsupportedError('Unsupported message type: ${data.runtimeType}');
-    });
-
-  // Start the helper isolate.
-  await Isolate.spawn((SendPort sendPort) async {
-    final ReceivePort helperReceivePort = ReceivePort()
-      ..listen((dynamic data) {
-        // On the helper isolate listen to requests and respond to them.
-        if (data is _SumRequest) {
-          final int result = bindings.user.sum_long_running(data.a, data.b);
-          final _SumResponse response = _SumResponse(data.id, result);
-          sendPort.send(response);
-          return;
-        }
-        throw UnsupportedError('Unsupported message type: ${data.runtimeType}');
-      });
-
-    // Send the port to the main isolate on which we can receive requests.
-    sendPort.send(helperReceivePort.sendPort);
-  }, receivePort.sendPort);
-
-  // Wait until the helper isolate has sent us back the SendPort on which we
-  // can start sending requests.
-  return completer.future;
-}();
