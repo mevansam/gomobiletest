6a1da19930666dba4b0f7bbfd8f77a3c4039af7d
Mevan Samaratunga <mevansam@gmail.com>
2024-03-31 22:49:47 -0400

    set target to ensure macos simulator builds are tagged correctly

--- shared/flutter/user/src/go/clangwrap-ios.sh
+++ shared/flutter/user/src/go/clangwrap-ios.sh
@@ -10,10 +10,14 @@ CLANG=`xcrun --sdk $SDK --find clang`
 
 if [ "$GOARCH" == "amd64" ]; then
     CARCH="x86_64"
-    TARGET="$CARCH-apple-ios$SDK_VERSION"
 elif [ "$GOARCH" == "arm64" ]; then
     CARCH="arm64"
-    TARGET="$CARCH-apple-ios$SDK_VERSION-simulator"
 fi
 
-exec $CLANG -arch $CARCH -target $TARGET -isysroot $SDK_PATH -mios-version-min=${IPHONEOS_DEPLOYMENT_TARGET} "$@"
+if [ "$SDK" = "iphoneos" ]; then
+  export TARGET="-target $CARCH-apple-ios$SDK_VERSION"
+elif [ "$SDK" = "iphonesimulator" ]; then
+  export TARGET="-target $CARCH-apple-ios$SDK_VERSION-simulator"
+fi
+
+exec $CLANG -arch $CARCH $TARGET -isysroot $SDK_PATH -mios-version-min=${IPHONEOS_DEPLOYMENT_TARGET} "$@"
